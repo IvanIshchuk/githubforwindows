426d4f22594c9e5583268d6a03e8a3327e623751
Ivan Ishchuk <3164630@gmail.com>
2024-04-09 13:08:55 +0200

    Version 09.04.24

--- wordy.py
+++ wordy.py
@@ -1,3 +1,6 @@
 question = 'What is 3 plus 2 multiplied by 3?'
+if question[-1] != '?':
+    raise ValueError("syntax error")
+question = question[:-1]
 words = question.split()
-print(words[-1])
+print(question)
